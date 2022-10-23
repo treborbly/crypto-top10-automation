640d2b4987be24fcfcd7813543980c06de9a816e
treborbly <robertsimonbailey@gmail.com>
2022-10-23 23:01:29 +0800

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,35 @@
+
+
+URL = 'https://www.coingecko.com/'
+
+WEBDRIVER_PATH = "/Users/robertbailey/Documents/Developments/chromedriver"
+
+currency_dropdown = "(//span[@data-target='currency-dropdown.button'])[1]"
+
+currency_search = "(//div[@class='col-12 col-lg-5 mt-2']/input)[1]"
+
+currency_php = "(//span[contains(.,'Philippine Peso')])[2]"
+
+COINS = {
+    "Coin 1": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[1]",
+    "Coin 2": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[2]",
+    "Coin 3": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[3]",
+    "Coin 4": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[4]",
+    "Coin 5": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[5]",
+    "Coin 6": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[6]",
+    "Coin 7": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[7]",
+    "Coin 8": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[8]",
+    "Coin 9": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[9]",
+    "Coin 10": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[10]"
+}
+
+coin = "(//div[@class='mr-md-3 tw-pl-2 md:tw-mb-0 tw-text-xl tw-font-bold tw-mb-0'])"
+
+
+value = "(//span[@class='no-wrap'])[1]"
+
+
+trend = "(//span[@data-target='percent-change.percent'])[1]"
+
+
+
