a2b7462351cf5fb8553ee745981d4128589ffef5
treborbly <robertsimonbailey@gmail.com>
2022-10-26 02:57:21 +0800

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -10,18 +10,18 @@ currency_search = "(//div[@class='col-12 col-lg-5 mt-2']/input)[1]"
 
 currency_php = "(//span[contains(.,'Philippine Peso')])[2]"
 
-COINS = {
-    "Coin 1": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[1]",
-    "Coin 2": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[2]",
-    "Coin 3": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[3]",
-    "Coin 4": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[4]",
-    "Coin 5": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[5]",
-    "Coin 6": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[6]",
-    "Coin 7": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[7]",
-    "Coin 8": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[8]",
-    "Coin 9": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[9]",
-    "Coin 10": "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[10]"
-}
+COINS = [
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[1]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[2]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[3]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[4]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[5]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[6]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[7]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[8]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[9]",
+    "(//a[@class='tw-flex tw-items-start md:tw-flex-row tw-flex-col'])[10]"
+]
 
 coin = "(//div[@class='mr-md-3 tw-pl-2 md:tw-mb-0 tw-text-xl tw-font-bold tw-mb-0'])"
 
